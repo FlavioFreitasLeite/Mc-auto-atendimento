ee53a2063aee4dc4dd0a859b3418368f943d719a
Flávio Freitas <flavio.aquarelaparques@gmail.com>
2025-02-18 22:57:03 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+    return (  <div className="p-5 border border-red-500 rounded-xl">
+        <h1>product page</h1>
+        <Button>FWS</Button>
+        <input placeholder="Bora botar essa Api rodar"/> 
+    </div> );
+}
+ 
+export default ProductPage;
