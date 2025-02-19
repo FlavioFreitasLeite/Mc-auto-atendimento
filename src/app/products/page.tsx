b41a86abd72edd00bc365d1a305adfd3a7e2feb9
Flávio Freitas <flavio.aquarelaparques@gmail.com>
2025-02-18 23:16:03 -0300

    chore: add eslint plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 
 const ProductPage = () => {
-    return (  <div className="p-5 border border-red-500 rounded-xl">
+    return (  
+    <div className="p-5 border border-red-500 rounded-xl">
         <h1>product page</h1>
         <Button>FWS</Button>
         <input placeholder="Bora botar essa Api rodar"/> 
