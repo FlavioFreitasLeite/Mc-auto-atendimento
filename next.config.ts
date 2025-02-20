b69add0ea0ee747a6e90515ca0087330a220241d
Flávio Freitas <flavio.aquarelaparques@gmail.com>
2025-02-20 13:26:30 -0300

    feat: add initial restaurant page and manu page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
+
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: ["u9a6wmr3as.ufs.sh"], // Adicione seu domínio aqui
+  },
+};
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{hostname: "u9a6wmr3as.ufs.sh"}]
+  }
+
+ 
+
 };
 
 export default nextConfig;
---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,33 @@
+
+import { db } from "@/lib/prisma";
+
+import { notFound } from "next/navigation";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+    params: Promise<{ slug: string }>;   
+    searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const IsConsumptionMehodValid = (consumptionMethod: string) => {
+    return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+}
+const RestaurantMenuPageProps = async ({params, searchParams}: RestaurantMenuPageProps) => {
+    const { slug } = await params;
+    const { consumptionMethod } = await searchParams;
+    if (!IsConsumptionMehodValid(consumptionMethod)) {
+        return notFound();
+    }
+    const restaurant = await db.restaurant.findUnique({ where: { slug } });
+    if (!restaurant) {
+        return notFound
+    }
+    return (
+        <div>   
+            <RestaurantHeader restaurant={restaurant}/>
+        </div>
+    );
+}
+ 
+export default RestaurantMenuPageProps;
